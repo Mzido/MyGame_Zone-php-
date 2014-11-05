be1dde6b9052affefd9aa4b0220b52ba2c7e5abb
Mzido <rmzido@live.com>
2014-11-05 16:09:35 +0200

    Create success.php
    this page must appear after successfully logging in.

---
+++ success.php
@@ -0,0 +1,27 @@
+
+<?php
+echo "welcome to game zone";
+?>
+
+<html>
+<head>
+<style type="text/css">
+.style1 {
+	border-width: 0px;
+}
+</style>
+</head>
+<body style="background-color: #CC66FF">
+<center>
+<fieldset><legend>Our Games:</legend>
+<h1>Please choose a game to play below:</h1>
+
+<a href="game1.php">
+<img alt="click here to play the Guessing Game" class="style1" height="100" src="guessGameLink.jpg" width="200"></a>
+<a href="game2.php">
+<img alt="learners lisence game" class="style1" height="100" src="learnersLisenceGameLink.jpg" width="200"></a><p>
+</p>
+</fieldset>
+</center>
+</body>
+</html>
